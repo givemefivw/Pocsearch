ce27ce5fd9dc2c425c799ca0a5e0fde9d94755aa
givemefivw <2143889877@qq.com>
2022-05-16 15:44:31 +0800

    Update Pocsearch.py

--- Pocsearch.py
+++ Pocsearch.py
@@ -1,52 +1,35 @@
 import requests
 import argparse
-import sys 
+import sys
+requests.packages.urllib3.disable_warnings()
 
-
-def banner():
-
-    banner = '''
-______                                  _     
-| ___ \                                | |    
-| |_/ /__   ___ ___  ___  __ _ _ __ ___| |__  
-|  __/ _ \ / __/ __|/ _ \/ _` | '__/ __| '_ \ 
-| | | (_) | (__\__ \  __/ (_| | | | (__| | | |
-\_|  \___/ \___|___/\___|\__,_|_|  \___|_| |_|
-                                                                                         
-
-                  Author : Givemefivw
-                  Version: 0.1
-                  Descrip: Poc-in-github项目查询脚本，网络不通的情况下需要梯子，查询到CVE编号后访问链接即可                                    
-                                                      
-    '''
-    print(banner)
-
-def monitor(year,cve):
+def monitor(search):
+    year = search[4:8]
+    number = search[9:]
+    cve = search[0:3]
     url = "https://github.com/nomi-sec/PoC-in-GitHub/tree/master/{}".format(year)
     try:
-        res = requests.get(url,timeout=2)
-        if res.status_code == 200 and 'CVE-{}-{}'.format(year,cve) in res.text:
-            print("[+] CVE-{}-{} is in Github".format(year,cve))
+        res = requests.get(url,timeout=4,verify=False)
+        if res.status_code == 200 and search in res.text:
+            print("\n[+] {} is in Github\n".format(search))
 
-            print("[*] https://github.com/nomi-sec/PoC-in-GitHub/blob/master/{}".format(year) + "/CVE-{}-{}.json".format(year,cve))
+            print("[*] https://github.com/nomi-sec/PoC-in-GitHub/blob/master/{}".format(year) + "/{}.json\n".format(search))
         else:
-            print("[-] CVE-{}-{} is not in Github".format(year,cve))
+            print("[-] {} is not in Github".format(search))
     except Exception as e:
         print("[-] May be you need VPN ?")
+        print(e)
     except KeyboardInterrupt:
         sys.exit()
 
 def main():
     parser = argparse.ArgumentParser(description='PocSearch Help')
-    parser.add_argument('-y','--year',help='cve-year',default='')
-    parser.add_argument('-c','--cve',help='cve',default='')
+    parser.add_argument('-s','--search',help='choose cve number, such as CVE-2019-12725',default='')
     args = parser.parse_args()
 
-    if args.year and args.cve:
-        year = args.year
-        cve = args.cve
-        monitor(year,cve)
+    if args.search:
+        search = args.search
+        monitor(search)
 
 if __name__ == '__main__':
-    banner()
     main()
